a9672ae29459977fbcc6f2b377e75a387d9b2943
Lucien Fiorini <lucienfiorini@gmail.com>
2025-04-08 10:42:24 +0200

    add eval for common endgame

--- src/eval.c
+++ src/eval.c
@@ -1,5 +1,7 @@
 #include "eval.h"
 
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "bitboard.h"
@@ -17,10 +19,30 @@ int soldier_position_reward[NUM_SQUARE] = {
     0, 0, 0, 0, 0, 0, 0, 20,
 };
 
+const int center_reward[NUM_SQUARE] = {
+    0, 1, 2, 2, 2, 2, 1, 0,
+    2, 2, 3, 3, 3, 3, 2, 1,
+    2, 3, 4, 4, 4, 4, 3, 2,
+    2, 3, 4, 5, 5, 4, 3, 2,
+    2, 3, 4, 4, 4, 4, 3, 2,
+    1, 2, 3, 3, 3, 3, 2, 1,
+    0, 1, 2, 2, 2, 2, 1, 0,
+  };
+
+int king_endgame_position_reward[NUM_SQUARE] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 10, 10, 0, 0, 0,
+    0, 0, 20, 15, 200, 200, 0, 0,
+    0, 0, 20, 200, 200, 20, 0, 0,
+    0, 0, 200, 200, 20, 20, 0, 0,
+    0, 0, 0, 10, 10, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 20,
+};
+
 void eval_init() {
     for (Square i = SQ_A1; i <= SQ_H7; i++) {
         for (Square j = SQ_A1; j <= SQ_H7; j++) {
-            distance[i][j] = 13 - abs(sq_file(i) - sq_file(j)) + abs(sq_rank(i) - sq_rank(j));
+            distance[i][j] = 13 - (abs(sq_file(i) - sq_file(j)) + abs(sq_rank(i) - sq_rank(j)));
         }
     }
 }
@@ -49,22 +71,36 @@ int eval(Position *position) {
 
     int material_score = 0;
 
-    // Don't care about material in setup phase
-    if (position->ply >= 10) {
-        int num_white_solider = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_SOLDIER]);
-        int num_white_general = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_GENERAL]);
-        int num_white_king = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_KING]);
-        int num_black_solider = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_SOLDIER]);
-        int num_black_general = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_GENERAL]);
-        int num_black_king = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_KING]);
-
-        material_score = 900 * (num_white_general - num_black_general)
-                         + 100 * (num_white_solider - num_black_solider);
-    }
+    int num_white_solider = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_SOLDIER]);
+    int num_white_general = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_GENERAL]);
+    int num_white_king = bb_popcnt(position->pieces[COLOR_WHITE][PIECE_KING]);
+    int num_black_solider = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_SOLDIER]);
+    int num_black_general = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_GENERAL]);
+    int num_black_king = bb_popcnt(position->pieces[COLOR_BLACK][PIECE_KING]);
 
     Bitboard white_pieces = pieces_by_color(position, COLOR_WHITE) & BB_USED;
     Bitboard black_pieces = pieces_by_color(position, COLOR_BLACK) & BB_USED;
     Bitboard all_pieces = white_pieces | black_pieces;
+
+    Square white_king_pos = -1;
+    Square black_king_pos = -1;
+
+    // Precalculate king positions
+    Bitboard white_king_it = position->pieces[COLOR_WHITE][PIECE_KING] & BB_USED;
+    if (white_king_it) {
+        white_king_pos = bb_it_next(&white_king_it);
+    }
+
+    Bitboard black_king_it = position->pieces[COLOR_BLACK][PIECE_KING] & BB_USED;
+    if (black_king_it) {
+        black_king_pos = bb_it_next(&black_king_it);
+    }
+
+    // Don't care about material in setup phase
+    if (position->ply >= 10) {
+        material_score = 900 * (num_white_general - num_black_general)
+                         + 100 * (num_white_solider - num_black_solider);
+    }
 
     int white_mobility = mobility(position, COLOR_WHITE, white_pieces, all_pieces);
     int black_mobility = mobility(position, COLOR_BLACK, black_pieces, all_pieces);
@@ -87,28 +123,60 @@ int eval(Position *position) {
 
     int soldier_position_score = white_soldier_position_score - black_soldier_position_score;
 
-    Bitboard white_king_it = position->pieces[COLOR_WHITE][PIECE_KING] & BB_USED;
     int white_king_protection = 0;
-
-    if (white_king_it) {
-        Square sq = bb_it_next(&white_king_it);
-        white_king_protection = 15 * bb_popcnt(king_attacks(sq) & white_pieces);
-    }
-
-    Bitboard black_king_it = position->pieces[COLOR_BLACK][PIECE_KING] & BB_USED;
     int black_king_protection = 0;
 
-    if (black_king_it) {
-        Square sq = bb_it_next(&white_king_it);
-        int cnt = bb_popcnt(king_attacks(sq) & black_pieces);
+    if (num_white_king > 0) {
+        int cnt = 15 * bb_popcnt(king_attacks(white_king_pos) & white_pieces);
+        if (cnt <= 2) {
+            white_king_protection = 15 * cnt;
+        } else {
+            // After 2 pieces, each additional piece contributes less
+            white_king_protection = 15 * 2 + (cnt - 2) * 7;
+        }
+    }
+    if (num_black_king > 0) {
+        int cnt = 15 * bb_popcnt(king_attacks(black_king_pos) & black_pieces);
         if (cnt <= 2) {
             black_king_protection = 15 * cnt;
         } else {
-            black_king_protection = 15 * 2 + (cnt - 2) * 7; // After 2 pieces, each additional piece contributes less
+            // After 2 pieces, each additional piece contributes less
+            black_king_protection = 15 * 2 + (cnt - 2) * 7;
         }
     }
 
     int king_protection_score = white_king_protection - black_king_protection;
 
-    return (material_score + mobility_score + soldier_position_score + king_protection_score) * turn;
+    // Endgame
+    int lone_king_distance_white = 0;
+    int lone_king_distance_black = 0;
+
+    int lone_king_center_reward_white = 0;
+    int lone_king_center_reward_black = 0;
+
+    if (position->ply >= 10) {
+        if (num_black_king > 0 && num_white_king > 0) {
+            if (num_black_general == 0 && num_white_general > 0) {
+                lone_king_distance_white = distance[white_king_pos][black_king_pos] * 30;
+                lone_king_center_reward_black = center_reward[black_king_pos] * 10;
+            }
+
+            if (num_white_general == 0 && num_black_general > 0) {
+                lone_king_distance_black = distance[white_king_pos][black_king_pos] * 30;
+                lone_king_center_reward_white = center_reward[white_king_pos] * 10;
+            }
+        }
+    }
+
+    int lone_king_distance_score = lone_king_distance_white - lone_king_distance_black;
+    int lone_king_center_score = lone_king_center_reward_white - lone_king_center_reward_black;
+
+    return (
+               material_score
+               + mobility_score
+               + soldier_position_score
+               + king_protection_score
+               + lone_king_distance_score
+               + lone_king_center_score
+           ) * turn;
 };
