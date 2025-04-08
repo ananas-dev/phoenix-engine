6157632485ef40a03b5c90c91ab82dea7cd6a583
Lucien Fiorini <lucienfiorini@gmail.com>
2025-04-08 10:44:41 +0200

    add killer move and history heuristics and keep result from partial iteration

--- src/search.c
+++ src/search.c
@@ -2,7 +2,6 @@
 
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
@@ -18,7 +17,6 @@ int nodes_visited = 0;
 struct timeval start_time;
 double max_time;
 bool time_over;
-int search_depth;
 
 PackedMoveFlag tt_best_move_flag;
 Square tt_best_move_from;
@@ -37,6 +35,9 @@ PVLine pv_table[MAX_PLY];
 bool pv_sorting = true;
 bool follow_pv = true;
 
+Move killer_moves[2][MAX_PLY];
+int history[NUM_COLOR][NUM_SQUARE][NUM_SQUARE];
+
 int search_ply = 0;
 
 double get_elapsed_time() {
@@ -51,32 +52,49 @@ double get_elapsed_time() {
 // Sort order:
 // 1. TT move
 // 2. PV move
-// 3. Normal move
-int weight_move(Move move) {
+// 3. Killer move
+// 4. History
+int weight_move(Move move, Color side_to_move) {
     if (tt_best_move_sorting && move.from == tt_best_move_from && move.to == tt_best_move_to) {
         tt_best_move_sorting = false;
-        return 2;
+        return 69000;
     }
 
     if (pv_sorting) {
         Move pv_move = pv_table[0].moves[search_ply];
         if (pv_move.from == move.from && pv_move.to == move.to && pv_move.captures == move.captures) {
             pv_sorting = false;
-            return 1;
+            return 42000;
         }
+    }
+
+    // If move is not a capture we can use killer moves and history moves to improve
+    if (!bb_is_empty(move.captures)) {
+        Move killer_move_1 = killer_moves[0][search_ply];
+        Move killer_move_2 = killer_moves[1][search_ply];
+
+        if (killer_move_1.from == move.from && killer_move_1.to == move.to) {
+            return 6900;
+        }
+
+        if (killer_move_2.from == move.from && killer_move_2.to == move.to) {
+            return 4200;
+        }
+
+        return history[side_to_move][move.from][move.to];
     }
 
     return 0;
 }
 
 // Here we use insertion sort since the move list is small and almost sorted
-void sort_move_list(MoveList *move_list) {
+void sort_move_list(MoveList *move_list, Color side_to_move) {
     tt_best_move_sorting = (tt_best_move_flag != FLAG_NULL);
 
     int weights[move_list->size];
 
     for (int i = 0; i < move_list->size; i++) {
-        weights[i] = weight_move(move_list->moves[i]);
+        weights[i] = weight_move(move_list->moves[i], side_to_move);
     }
 
     for (int i = 1; i < move_list->size; i++) {
@@ -95,17 +113,13 @@ void sort_move_list(MoveList *move_list) {
     }
 }
 
-bool is_time_up(void) {
-    // Check time every 1000 nodes or so to avoid expensive clock() calls
+void update_clock(void) {
     double elapsed = get_elapsed_time();
-
 
     // Use 95% of allocated time to ensure we return before timeout
     if (elapsed >= max_time * 0.99) {
         time_over = true;
-        return true;
     }
-    return false;
 }
 
 int alpha_beta(Position *position, int depth, int alpha, int beta);
@@ -114,6 +128,8 @@ Move search(Position *position, double max_time_seconds) {
     search_ply = 0;
 
     memset(pv_table, 0, sizeof(pv_table));
+    memset(killer_moves, 0, sizeof(killer_moves));
+    memset(history, 0, sizeof(history));
 
     follow_pv = false;
     pv_sorting = false;
@@ -137,34 +153,30 @@ Move search(Position *position, double max_time_seconds) {
 
         int score = alpha_beta(position, depth, alpha, beta);
 
-        if (!time_over) {
-            best_move = pv_table[0].moves[0];
+        best_move = pv_table[0].moves[0];
 
-            printf("Depth=%d, Score=%.2f\n", depth, (float)score/100.0f);
+        printf("Depth=%d, Score=%.2f\n", depth, (float)score/100.0f);
 
-            for (int i = 0; i < pv_table[0].size; i++) {
-                move_print(pv_table[0].moves[i]);
-                printf(" ");
-            }
+        for (int i = 0; i < pv_table[0].size; i++) {
+            move_print(pv_table[0].moves[i]);
+            printf(" ");
+        }
 
-            printf("\n");
+        printf("\n");
 
-            // Return early if mate is found
-            if (score >= INF - MAX_PLY) {
-                return best_move;
-            }
+        // Return early if mate is found
+        if (score >= INF - MAX_PLY) {
+            return best_move;
         }
 
     }
 
-    return best_move;
+    return pv_table[0].moves[0];
 }
 
 int quiesce(Position *position, int alpha, int beta) {
     if ((nodes_visited & 2047) == 0) {
-        if (is_time_up()) {
-            return 0;
-        }
+        update_clock();
     }
 
     if (position_is_game_over(position)) {
@@ -182,9 +194,10 @@ int quiesce(Position *position, int alpha, int beta) {
 
     MoveList move_list;
     move_list.size = 0;
+
     legal_moves(position, &move_list);
     tt_best_move_flag = FLAG_NULL;
-    sort_move_list(&move_list);
+    sort_move_list(&move_list, position->side_to_move);
 
     for (int i = 0; i < move_list.size; i++) {
         Move move = move_list.moves[i];
@@ -205,9 +218,17 @@ int quiesce(Position *position, int alpha, int beta) {
             }
         }
 
+        search_ply++;
+
         Position new_position = make_move(position, move);
 
         int score = -quiesce(&new_position, -beta, -alpha);
+
+        search_ply--;
+
+        if (time_over) {
+            return 0;
+        }
 
         if (score > alpha) {
             alpha = score;
@@ -228,11 +249,9 @@ int alpha_beta(Position *position, int depth, int alpha, int beta) {
         return INF - search_ply;
     }
 
-    // Check every 1000 nodes
+    // Update clock from time to time
     if ((nodes_visited & 2047) == 0) {
-        if (is_time_up()) {
-            return 0;
-        }
+        update_clock();
     }
 
     PackedMove tt_best_move = NULL_PACKED_MOVE;
@@ -269,13 +288,7 @@ int alpha_beta(Position *position, int depth, int alpha, int beta) {
         }
     }
 
-    sort_move_list(&move_list);
-
-    if (follow_pv) {
-        Move fst = move_list.moves[0];
-        Move snd = pv_table[0].moves[search_ply];
-        assert(fst.from == snd.from && fst.to == snd.to);
-    }
+    sort_move_list(&move_list, position->side_to_move);
 
     Move best_move = {0};
 
@@ -303,6 +316,10 @@ int alpha_beta(Position *position, int depth, int alpha, int beta) {
 
         search_ply--;
 
+        if (time_over) {
+            return 0;
+        }
+
         if (score > alpha) {
             tt_entry_type = ENTRY_TYPE_EXACT;
             alpha = score;
@@ -318,7 +335,15 @@ int alpha_beta(Position *position, int depth, int alpha, int beta) {
 
             if (score >= beta) {
                 PackedMove best_move_packed = packed_move_new(FLAG_NORMAL, best_move.from, best_move.to);
-                tt_set(position, depth, beta, best_move_packed, ENTRY_TYPE_BETA);
+                tt_set(position, depth, beta, ENTRY_TYPE_BETA, best_move_packed);
+
+                if (bb_is_empty(move.captures)) {
+                    killer_moves[1][search_ply] = killer_moves[0][search_ply];
+                    killer_moves[0][search_ply] = move;
+                    history[position->side_to_move][move.from][move.to] += depth * depth;
+                }
+
+
                 return beta;
             }
 
